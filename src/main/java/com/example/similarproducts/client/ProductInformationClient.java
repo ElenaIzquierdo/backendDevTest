28a9ad60aac5617edefc15b0fb05c2c6f6d8908c
ElenaIzquierdo <eizquierdograu@gmail.com>
2023-05-10 20:51:20 +0200

    Add cache in front of external api call and reduce time out to 1s

--- src/main/java/com/example/similarproducts/client/ProductInformationClient.java
+++ src/main/java/com/example/similarproducts/client/ProductInformationClient.java
@@ -1,19 +1,29 @@
 package com.example.similarproducts.client;
 
 import com.example.similarproducts.dto.Product;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
 
+import java.time.Duration;
 import java.util.Optional;
 
 
 @Service
 public class ProductInformationClient {
 
+    @Cacheable("productDetails")
     public Optional<Product> getProductInformation(String productId) {
         String url = "http://localhost:3001/product/" + productId;
 
-        WebClient.Builder builder = WebClient.builder();
+        HttpClient client = HttpClient.create()
+                .responseTimeout(Duration.ofSeconds(1));
+
+        WebClient.Builder builder = WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(client));
+
         Product response = builder.build()
                 .get()
                 .uri(url)
--- src/main/java/com/example/similarproducts/client/SimilarProductIdsClient.java
+++ src/main/java/com/example/similarproducts/client/SimilarProductIdsClient.java
@@ -1,17 +1,26 @@
 package com.example.similarproducts.client;
 
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
 
+import java.time.Duration;
 import java.util.List;
 
 @Service
 public class SimilarProductIdsClient {
 
+    @Cacheable("similarProducts")
     public List<Integer> getSimilarProductIds(String productId) {
         String url = "http://localhost:3001/product/" + productId + "/similarids";
 
-        WebClient.Builder builder = WebClient.builder();
+        HttpClient client = HttpClient.create()
+                .responseTimeout(Duration.ofSeconds(1));
+
+        WebClient.Builder builder = WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(client));
 
         return builder.build()
                 .get()
