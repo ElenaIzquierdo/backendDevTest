7d0182c6684409ea6937251e24a8d4196517f955
ElenaIzquierdo <eizquierdograu@gmail.com>
2023-05-10 22:54:18 +0200

    Add 1 second of time out for the external API requests

--- src/main/java/com/example/similarproducts/client/ProductInformationClient.java
+++ src/main/java/com/example/similarproducts/client/ProductInformationClient.java
@@ -1,7 +1,10 @@
 package com.example.similarproducts.client;
 
 import com.example.similarproducts.dto.Product;
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.client.reactive.ClientHttpConnector;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -9,26 +12,26 @@ import reactor.netty.http.client.HttpClient;
 
 import java.time.Duration;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 
 @Service
 public class ProductInformationClient {
 
+    public final int ONE_SECOND = 1000;
+
     @Cacheable("productDetails")
     public Optional<Product> getProductInformation(String productId) {
         String url = "http://localhost:3001/product/" + productId;
 
-        HttpClient client = HttpClient.create()
-                .responseTimeout(Duration.ofSeconds(1));
-
-        WebClient.Builder builder = WebClient.builder()
-                .clientConnector(new ReactorClientHttpConnector(client));
+        WebClient.Builder builder = createWebClientWithConnectAndReadTimeOuts();
 
         Product response = builder.build()
                 .get()
                 .uri(url)
                 .retrieve()
                 .bodyToMono(Product.class)
+                .timeout(Duration.ofSeconds(1))
                 .block();
 
         if(response == null) {
@@ -36,5 +39,18 @@ public class ProductInformationClient {
         }
 
         return Optional.of(response);
+    }
+
+    private WebClient.Builder createWebClientWithConnectAndReadTimeOuts() {
+        HttpClient httpClient = HttpClient.create()
+                .tcpConfiguration(tcpClient -> {
+                    tcpClient = tcpClient.option(ChannelOption.CONNECT_TIMEOUT_MILLIS, ONE_SECOND);
+                    tcpClient = tcpClient.doOnConnected(conn -> conn
+                            .addHandlerLast(new ReadTimeoutHandler(ONE_SECOND, TimeUnit.SECONDS)));
+                    return tcpClient;
+                });
+
+        ClientHttpConnector connector = new ReactorClientHttpConnector(httpClient);
+        return WebClient.builder().clientConnector(connector);
     }
 }
--- src/main/java/com/example/similarproducts/client/SimilarProductIdsClient.java
+++ src/main/java/com/example/similarproducts/client/SimilarProductIdsClient.java
@@ -1,6 +1,9 @@
 package com.example.similarproducts.client;
 
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.client.reactive.ClientHttpConnector;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -8,27 +11,40 @@ import reactor.netty.http.client.HttpClient;
 
 import java.time.Duration;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class SimilarProductIdsClient {
+
+    public static final int ONE_SECOND = 1000;
+
 
     @Cacheable("similarProducts")
     public List<Integer> getSimilarProductIds(String productId) {
         String url = "http://localhost:3001/product/" + productId + "/similarids";
 
-        HttpClient client = HttpClient.create()
-                .responseTimeout(Duration.ofSeconds(1));
-
-        WebClient.Builder builder = WebClient.builder()
-                .clientConnector(new ReactorClientHttpConnector(client));
+        WebClient.Builder builder = createWebClientWithConnectAndReadTimeOuts();
 
         return builder.build()
                 .get()
                 .uri(url)
                 .retrieve()
                 .bodyToFlux(Integer.class)
+                .timeout(Duration.ofSeconds(1))
                 .collectList()
                 .block();
+    }
 
+    private WebClient.Builder createWebClientWithConnectAndReadTimeOuts() {
+        HttpClient httpClient = HttpClient.create()
+                .tcpConfiguration(tcpClient -> {
+                    tcpClient = tcpClient.option(ChannelOption.CONNECT_TIMEOUT_MILLIS, ONE_SECOND);
+                    tcpClient = tcpClient.doOnConnected(conn -> conn
+                            .addHandlerLast(new ReadTimeoutHandler(ONE_SECOND, TimeUnit.SECONDS)));
+                    return tcpClient;
+                });
+
+        ClientHttpConnector connector = new ReactorClientHttpConnector(httpClient);
+        return WebClient.builder().clientConnector(connector);
     }
 }
