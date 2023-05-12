f7a8e2caed0c322787f4f94f175754958f2daf10
ElenaIzquierdo <eizquierdograu@gmail.com>
2023-05-12 21:58:41 +0200

    Add logs and throw run time exception if call to external api times out

--- src/main/java/com/example/similarproducts/client/ProductInformationClient.java
+++ src/main/java/com/example/similarproducts/client/ProductInformationClient.java
@@ -1,10 +1,12 @@
 package com.example.similarproducts.client;
 
 import com.example.similarproducts.dto.Product;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientRequestException;
 import reactor.netty.http.client.HttpClient;
 
 import java.time.Duration;
@@ -12,29 +14,37 @@ import java.util.Optional;
 
 
 @Service
+@Slf4j
 public class ProductInformationClient {
 
     @Cacheable("productDetails")
     public Optional<Product> getProductInformation(String productId) {
         String url = "http://host.docker.internal:3001/product/" + productId;
+        try {
+            log.info(String.format("Starting request to %s", url));
+            HttpClient client = HttpClient.create()
+                    .responseTimeout(Duration.ofSeconds(1));
 
-        HttpClient client = HttpClient.create()
-                .responseTimeout(Duration.ofMillis(500));
+            WebClient.Builder builder = WebClient.builder()
+                    .clientConnector(new ReactorClientHttpConnector(client));
 
-        WebClient.Builder builder = WebClient.builder()
-                .clientConnector(new ReactorClientHttpConnector(client));
+            Product response = builder.build()
+                    .get()
+                    .uri(url)
+                    .retrieve()
+                    .bodyToMono(Product.class)
+                    .block();
 
-        Product response = builder.build()
-                .get()
-                .uri(url)
-                .retrieve()
-                .bodyToMono(Product.class)
-                .block();
+            if(response == null) {
+                return Optional.empty();
+            }
 
-        if(response == null) {
-            return Optional.empty();
+            return Optional.of(response);
+        } catch(WebClientRequestException exception) {
+            String message = String.format("Call to %s failed due to time out", url);
+            log.error(message);
+            throw new RuntimeException(message);
         }
 
-        return Optional.of(response);
     }
 }
--- src/main/java/com/example/similarproducts/client/SimilarProductIdsClient.java
+++ src/main/java/com/example/similarproducts/client/SimilarProductIdsClient.java
@@ -1,34 +1,43 @@
 package com.example.similarproducts.client;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientRequestException;
 import reactor.netty.http.client.HttpClient;
 
 import java.time.Duration;
 import java.util.List;
 
 @Service
+@Slf4j
 public class SimilarProductIdsClient {
 
     @Cacheable("similarProducts")
     public List<Integer> getSimilarProductIds(String productId) {
         String url = "http://host.docker.internal:3001/product/" + productId + "/similarids";
+        try {
+            log.info(String.format("Starting request to %s", url));
+            HttpClient client = HttpClient.create()
+                    .responseTimeout(Duration.ofSeconds(1));
 
-        HttpClient client = HttpClient.create()
-                .responseTimeout(Duration.ofMillis(500));
+            WebClient.Builder builder = WebClient.builder()
+                    .clientConnector(new ReactorClientHttpConnector(client));
 
-        WebClient.Builder builder = WebClient.builder()
-                .clientConnector(new ReactorClientHttpConnector(client));
-
-        return builder.build()
-                .get()
-                .uri(url)
-                .retrieve()
-                .bodyToFlux(Integer.class)
-                .collectList()
-                .block();
+            return builder.build()
+                    .get()
+                    .uri(url)
+                    .retrieve()
+                    .bodyToFlux(Integer.class)
+                    .collectList()
+                    .block();
+        } catch(WebClientRequestException exception) {
+            String message = String.format("Call to %s failed due to time out", url);
+            log.error(message);
+            throw new RuntimeException(message);
+        }
 
     }
 }
