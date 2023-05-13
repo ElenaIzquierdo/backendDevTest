257cc1fa55fd1fcc5b3bdaa8685bdb499cb64f53
ElenaIzquierdo <eizquierdograu@gmail.com>
2023-05-13 16:59:23 +0200

    Remove unused imports

--- src/main/java/com/example/similarproducts/client/SimilarProductIdsClient.java
+++ src/main/java/com/example/similarproducts/client/SimilarProductIdsClient.java
@@ -1,6 +1,5 @@
 package com.example.similarproducts.client;
 
-import com.example.similarproducts.usecase.SimilarProductsUseCase;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.Cacheable;
